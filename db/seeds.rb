f6508ed42fcc6ead37d0126456a424c6bd8f48da
benjamin fredj <bfredj@hotmail.com>
2015-08-19 15:14:50 +0200

    creation controllers: ok, creation seed: ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+restaurants = [
+{
+  name: "Le dindon en laisse",
+  address: "25 rue du petit musc",
+  phone_number: "0146591500",
+  category: "french"
+},
+{
+  name: "East Mama",
+  address: "45 rue du faubourg saint antoine",
+  phone_number: "0146591501",
+  category: "italian"
+},
+{
+  name: "Le Benkay",
+  address: "61 Quai de Grenelle",
+  phone_number: "0146591502",
+  category: "japanese"
+},
+{
+  name: "WenZhou",
+  address: "24 Rue de Belleville",
+  phone_number: "0146591503",
+  category: "chinese"
+},
+{
+  name: "Léon de Bruxelles",
+  address: "3 Boulevard Beaumarchais",
+  phone_number: "0146591504",
+  category: "belgian"
+}
+]
+
+restaurants.each do |restaurant|
+  Restaurant.create(restaurant)
+end
