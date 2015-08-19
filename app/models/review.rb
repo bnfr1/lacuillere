bd826b41608b4b304b694716daa607480b37f017
benjamin fredj <bfredj@hotmail.com>
2015-08-19 14:13:53 +0200

    validates: ok

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, presence: true, numericality: true, inclusion: { in: (0..5), allow_nil: false}
+  validates :restaurant, presence: true
 end
+
+
